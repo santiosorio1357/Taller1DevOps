d9b53b4813dab7ef3e7b35729ab0b181088bb8c2
Santiago Osorio <santiosorio1357@gmail.com>
2024-03-03 22:07:27 -0500

    Mensaje de commit para los cambios en el submódulo

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -3,13 +3,33 @@ import { InjectModel } from '@nestjs/mongoose';
 
 import { Todo } from './schemas/todo.schema';
 import { Model } from 'mongoose';
+import { CreateTodoDto } from './dtos/create-todo.dto';
+import { UpdateTodoDto } from './dtos/update-todo.dto';
 
 @Injectable()
 export class TodoService {
-    constructor(@InjectModel(Todo.name) private todoModeL: Model<Todo>) {}
+    constructor(@InjectModel(Todo.name) private todoModeL: Model<Todo>) { }
 
-    async create(todo: any){
+    async create(todo: CreateTodoDto) {
         const createdTodoo = new this.todoModeL(todo)
         return createdTodoo.save()
+    }
+
+    async update(id: string, todo: UpdateTodoDto) {
+        return this.todoModeL.findByIdAndUpdate(id, todo, {
+            new: true,
+        }).exec()
+    }
+
+    async findAll(){
+        return this.todoModeL.find().exec()
+    }
+
+    async findOne(id: string){
+        return this.todoModeL.findById(id).exec()
+    }
+
+    async delete(id: string){
+        return this.todoModeL.findByIdAndDelete(id).exec()
     }
 }
